2d94387ffcc704fa7480d7c7b4fbc11abe9d6623
Garrett Fraser <Fraser578@gmail.com>
2018-10-24 17:16:08 -0400

    Added update script

---
+++ dbo/Stored Procedures/spUpdateCard.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Garrett Fraser
+-- Create date: 2018-10-11
+-- Description:	Stored procedure to update cards
+-- =============================================
+CREATE PROCEDURE [dbo].[spUpdateCard] 
+	@DeckId INT,
+	@Question VARCHAR(MAX),
+	@Answer VARCHAR(MAX)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    UPDATE dbo.Cards
+	SET @DeckId = DeckId, @Question = Question, @Answer = Answer
+END
