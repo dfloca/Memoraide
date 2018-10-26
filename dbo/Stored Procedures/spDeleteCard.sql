86104233e201528f6a150863946e81fb90d4e6f4
dfloca <dfloca@users.noreply.github.com>
2018-10-25 20:55:41 -0400

    Update dbo/Stored Procedures/spDeleteCard.sql

---
+++ dbo/Stored Procedures/spDeleteCard.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Daniel Floca
+-- Create date: 2018-10-25
+-- Description:	Stored procedure to delete card
+-- =============================================
+CREATE PROCEDURE [dbo].[spDeleteCard]
+	@CardId INT
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	UPDATE dbo.Cards
+	SET IsDeleted = 1, DateDeleted = GETDATE()
+	WHERE Id = @CardId;
+
+END
+GO
