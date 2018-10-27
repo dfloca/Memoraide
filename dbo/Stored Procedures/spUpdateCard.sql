de26fc2c95dd1bc696bdcd119efec54717e96416
dfloca <dfloca@users.noreply.github.com>
2018-10-26 22:23:10 -0400

    Added update script

--- dbo/Stored Procedures/spUpdateCard.sql
+++ dbo/Stored Procedures/spUpdateCard.sql
@@ -1,17 +1,25 @@
 ﻿-- =============================================
--- Author:		Garrett Fraser
--- Create date: 2018-10-11
+-- Author:		Daniel Floca
+-- Create date:	2018-10-26
 -- Description:	Stored procedure to update cards
 -- =============================================
 CREATE PROCEDURE [dbo].[spUpdateCard] 
 	@DeckId INT,
 	@Question VARCHAR(MAX),
-	@Answer VARCHAR(MAX)
+	@Answer VARCHAR(MAX),
+	@IsDeleted BIT
 AS
 BEGIN
-
+ 	
 	SET NOCOUNT ON;
 
     UPDATE dbo.Cards
-	SET @DeckId = DeckId, @Question = Question, @Answer = Answer
-END
+	SET DeckId = @DeckId,
+		Question = @Question,
+		Answer = @Answer,
+		IsDeleted = @IsDeleted,
+		DateDeleted = CASE 
+						WHEN @IsDeleted = 1 THEN GETDATE()
+						ELSE NULL
+					  END
+END 
