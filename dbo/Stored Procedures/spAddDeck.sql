b21bd82b5bee70dae882b16d25f161afbd03c4ca
dfloca <dfloca@users.noreply.github.com>
2018-10-26 22:39:46 -0400

    Update dbo/Stored Procedures/spAddDeck.sql

---
+++ dbo/Stored Procedures/spAddDeck.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Author:		Daniel Floca
+-- Create date: 2018-10-26
+-- Description:	Stored procedure to add new decks
+-- =============================================
+CREATE PROCEDURE spAddDeck 
+	@Name VARCHAR(50),
+	@UserId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    INSERT INTO dbo.Decks ([Name], UserId, DateCreated)
+	VALUES (@Name, @UserId, GETDATE())
+END
