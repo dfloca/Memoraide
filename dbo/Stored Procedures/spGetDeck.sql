13b7427ede4d357304e5a4e7728ee2acf5f6bd36
dfloca <dfloca@users.noreply.github.com>
2018-10-26 22:56:09 -0400

    Update dbo/Stored Procedures/spGetDeck.sql

---
+++ dbo/Stored Procedures/spGetDeck.sql
@@ -0,0 +1,19 @@
+﻿
+-- =============================================
+-- Author:		Daniel Floca
+-- Create date: 2018-10-26
+-- Description:	Stored procedure to retrieve a deck
+-- =============================================
+CREATE PROCEDURE [dbo].[spGetDeck] 
+	@DeckId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    SELECT *
+	FROM dbo.Cards
+	WHERE Id = @DeckId AND (IsDeleted = 0 OR IsDeleted IS NULL)
+	  
+END
+
