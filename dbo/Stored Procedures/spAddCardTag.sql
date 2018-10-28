acc86c2a36d45b8cb38025c83cc0647b012f7728
Garrett Fraser <Fraser578@gmail.com>
2018-10-28 14:40:49 -0400

    Added create tag stored proc

---
+++ dbo/Stored Procedures/spAddCardTag.sql
@@ -0,0 +1,16 @@
+﻿-- ===================================================
+-- Author:		Garrett Fraser
+-- Create date: 2018-10-11
+-- Description:	Stored procedure to add new card tags
+-- ===================================================
+CREATE PROCEDURE [dbo].[spAddCardTag] 
+	@CardId INT,
+	@Tag VARCHAR(MAX)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    INSERT INTO dbo.CardTags (CardId, Tag)
+	VALUES (@CardId, @Tag)
+END
