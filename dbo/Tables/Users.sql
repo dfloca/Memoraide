e20335852e992b317318c0bb89ecc968bbcea036
dfloca <dfloca@users.noreply.github.com>
2018-11-10 16:50:09 -0500

    updates to users table and data script

--- dbo/Tables/Users.sql
+++ dbo/Tables/Users.sql
@@ -3,10 +3,13 @@
     [FirstName]   VARCHAR (50)   NOT NULL,
     [LastName]    VARCHAR (50)   NOT NULL,
     [Email]       NVARCHAR (100) NOT NULL,
+    [Username]	  VARCHAR(50) NOT NULL, 
+    [Password]	  NVARCHAR(MAX) NOT NULL, 
+    [DateCreated] DATETIME		 NULL, 
     [IsBanned]    BIT            NULL,
-    [BannedDate]  DATETIME       NULL,
+    [DateBanned]  DATETIME       NULL,
     [IsDeleted]   BIT            NULL,
-    [DeletedDate] DATETIME       NULL,
+    [DateDeleted] DATETIME       NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
