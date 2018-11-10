afc1af21a58156ab3bc3a368a35927faa7a1261d
dfloca <dfloca@users.noreply.github.com>
2018-11-10 12:24:45 -0500

    Created spGetUser sproc

---
+++ dbo/Stored Procedures/spGetUser.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- Author:		Daniel Floca
+-- Create date: 2018-11-10
+-- Description:	Stored procedure to retrieve a user
+-- =============================================
+CREATE PROCEDURE [dbo].[spGetUser] 
+	@UserId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    SELECT *
+	FROM dbo.Users
+	WHERE Id = @UserId AND (IsDeleted = 0 OR IsDeleted IS NULL)
+	  
+END
