c8d086a00d85c003ea9d317a8e92207f6ee41c27
Garrett Fraser <Fraser578@gmail.com>
2018-11-14 13:36:45 -0500

    Added ban functionality

---
+++ dbo/Stored Procedures/spBanUser.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		Garrett Fraser
+-- Create date: 2018-11-10
+-- Description:	Stored procedure to ban users
+-- =============================================
+CREATE PROCEDURE [dbo].[spBanUser] 
+	@UserId INT
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    UPDATE dbo.Users
+	SET IsBanned = 1,
+		DateBanned = GETDATE()
+	WHERE Id = @UserId
+END
+
+
