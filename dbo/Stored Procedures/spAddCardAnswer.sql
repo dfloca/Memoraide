482ae3bad510c4678c0c2c73fb2abe95d6bb383f
Garrett Fraser <Fraser578@gmail.com>
2018-11-25 15:35:44 -0500

    Added review functionality

---
+++ dbo/Stored Procedures/spAddCardAnswer.sql
@@ -0,0 +1,20 @@
+﻿-- =============================================
+-- Author:		Garrett Fraser
+-- Create date: 2018-10-11
+-- Description:	Stored procedure to add new card answers
+-- =============================================
+CREATE PROCEDURE [dbo].[spAddCardAnswer] 
+	@UserId INT,
+	@DeckId INT,
+	@CorrectAnswer BIT,
+	@NextReviewDate DateTime
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+    INSERT INTO dbo.CardAnswers(UserId, DeckId, CorrectAnswer, DateAnswered, NextReviewDate)
+	VALUES (@UserId, @DeckId, @CorrectAnswer, GETDATE(), @NextReviewDate)
+END
+
+
---
+++ dbo/Tables/CardAnswers.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[CardAnswers](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[DeckId] [int] NOT NULL,
+	[CorrectAnswer] [bit] NOT NULL,
+	[DateAnswered] [datetime] NOT NULL,
+	[NextReviewDate] [datetime] NOT NULL,
+	PRIMARY KEY CLUSTERED ([Id] ASC)
+	WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
